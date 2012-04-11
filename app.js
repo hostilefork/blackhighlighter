519dba779eccc7d7c055e9e9d3886f771d840366
Hostile Fork <hostilefork@gmail.com>
2012-04-11 07:36:42 -0500

    Begin project of mothballing the django code for node.js, make fresh copies of the templates and media

---
+++ app.js
@@ -0,0 +1,109 @@
+require.paths.unshift('./node_modules');
+
+// This hello-world-with-mongodb-tracking is a hybrid
+
+// Mongodb interface from 
+// http://blog.mongodb.org/post/6587009156/cloudfoundry-mongodb-and-nodejs
+
+// Express framework interface from slide 99 of "Becoming a Node.js ninja on CloudFoundry"
+// http://www.slideshare.net/chanezon/cloud-foundry-open-tour-beijing-becoming-a-nodejs-ninja-on-cloud-foundry
+
+
+if(process.env.VCAP_SERVICES){
+  var env = JSON.parse(process.env.VCAP_SERVICES);
+  var mongo = env['mongodb-1.8'][0]['credentials'];
+}
+else{
+  var mongo = {
+    "hostname":"localhost",
+    "port":27017,
+    "username":"",
+    "password":"", 
+    "name":"",
+    "db":""
+  }
+}
+
+var generate_mongo_url = function(obj){
+  obj.hostname = (obj.hostname || 'localhost');
+  obj.port = (obj.port || 27017);
+  obj.db = (obj.db || 'test');
+
+  if(obj.username && obj.password){
+    return "mongodb://" + obj.username + ":" + obj.password + "@" + obj.hostname + ":" + obj.port + "/" + obj.db;
+  }
+  else{
+    return "mongodb://" + obj.hostname + ":" + obj.port + "/" + obj.db;
+  }
+}
+
+var mongourl = generate_mongo_url(mongo);
+
+/* Http Variables */
+var port = (process.env.VMC_APP_PORT || 3000);
+var host = (process.env.VCAP_APP_HOST || 'localhost');
+var http = require('http');
+
+var record_visit = function(req, res){
+  /* Connect to the DB and auth */
+  require('mongodb').connect(mongourl, function(err, conn){
+    conn.collection('ips', function(err, coll){
+      /* Simple object to insert: ip address and date */
+      object_to_insert = { 'ip': req.connection.remoteAddress, 'ts': new Date() };
+
+      /* Insert the object then print in response */
+      /* Note the _id has been created */
+      coll.insert( object_to_insert, {safe:true}, function(err){
+        if(err) { console.log(err.stack); }
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.write(JSON.stringify(object_to_insert));
+        res.end('\n');
+      });
+    });
+  });
+}
+
+var print_visits = function(req, res){
+  /* Connect to the DB and auth */
+  require('mongodb').connect(mongourl, function(err, conn){
+    conn.collection('ips', function(err, coll){
+      coll.find({}, {limit:10, sort:[['_id','desc']]}, function(err, cursor){
+        cursor.toArray(function(err, items){
+          res.writeHead(200, {'Content-Type': 'text/plain'});
+          for(i=0; i<items.length;i++){
+            res.write(JSON.stringify(items[i]) + "\n");
+          }
+          res.end();
+        });
+      });
+    });
+  });
+}
+
+var express = require('express');
+var app = express.createServer();
+
+app.configure('development', function() {
+	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function() {
+	app.use(express.errorHandler());
+});
+
+app.get('/', function(req, res){
+    res.redirect('/helloworld')
+});
+ 
+app.get('/helloworld', function(req, res){
+	// send is part of express and "ends" the response
+	// http://expressjs.com/guide.html#res.send%28%29
+	/* res.send('Hello World...'); */
+	record_visit(req, res);
+});
+
+app.get('/history', function(req, res){
+	print_visits(req, res);
+});
+ 
+app.listen(port, host);
