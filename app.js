hostilefork/blackhighlighter

commit 36ff6937caef71f8fb3db4da6c7c15d94b3f9840

UI polish work stabilizing edit box positions - closes #10 and #13

app.js
@@ -352,7 +352,11 @@ app.get('/docs/*', function(req, res) {
 // entirely in JavaScript on the client's machine.  The /commit/ HTTP POST
 // handler does the actual server-side work of saving the document.
 app.get('/write/$', function (req, res) {
-    res.render('write', { MAIN_SCRIPT: "write" });
+    res.render('write', {
+    	MAIN_SCRIPT: "write"
+    	, HOSTING_SERVICE: process.env.HOSTING_SERVICE
+    	, HOSTING_SERVICE_URL: process.env.HOSTING_SERVICE_URL
+    });
 });
 
 
@@ -417,8 +421,7 @@ function generateHtmlFromCommitAndReveals(commit, reveals) {
 				// more clever?  e.g. we could add a method onto the element
 				// or keep a sidestructure
 				var placeholder = 
-					'<span class="placeholder protected" title="'
-					+ commitSpan.sha256 + '">'
+					'<span class="placeholder protected">'
 					+ placeholderString + '</span>';
 
 				result += placeholder;
@@ -511,6 +514,8 @@ function showOrVerify(req, res, tabstate) {
 		// 6: Generate response HTML
 		res.render('read', {
 			MAIN_SCRIPT: 'read'
+			, HOSTING_SERVICE: process.env.HOSTING_SERVICE
+			, HOSTING_SERVICE_URL: process.env.HOSTING_SERVICE_URL
 			, commit_id: commit_id
 			, all_certificates: generateCertificateStubsFromCommit(commit)
 			, tabstate: tabstate

public/client-common.js
@@ -15,7 +15,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
-//   See http://hostilefork.com/blackhighlighter for documentation.
+//   See http://blackhighlighter.hostilefork.com for documentation.
 //
 
 
@@ -121,6 +121,49 @@ define(['jquery', 'underscore'], function($, _){
 			} else if (document.selection) {
 				document.selection.empty();
 			}
-		}
+		},
+
+		// http://stackoverflow.com/a/12463110/211160
+		//
+		getHiddenHeightForWidth: function(element, width) {
+		    var $temp = $(element).clone()
+		 		.css('position','absolute')
+		   		.css('height','auto').css('width', width + 'px')
+		  	  	// inject right into parent element so all the css applies
+		  	  	// (yes, i know, except the :first-child and other pseudo stuff
+			    .appendTo($(element).parent())
+			    .css('left','-10000em')
+			    .show();
+
+		    h = $temp.height();
+		    $temp.remove();
+		    return h;
+		},
+
+		resizeListener: function(eventObj) {
+			// We want some leading text before the boxes containing data, but we
+			// want them to be the same size.  We can get their height by default
+			// and then style them to have the height of the maximum of any of them.
+
+			$leads = $('#tabs > div.tabs-content div.leading-section');
+
+			$leads.css('height', 'auto');
+
+			var maxHeight = undefined;
+
+			$leads.each(function(idx, el) {
+				var $el = $(el);
+				var hiddenHeight = $el.actual('height');
+				if (!maxHeight) {
+					maxHeight = hiddenHeight;
+				} else {
+					if (hiddenHeight > maxHeight) {
+						maxHeight = hiddenHeight;
+					}
+				}
+			});
+
+	    	$leads.height(maxHeight);
+	    }
 	}
 });

public/demo.css
@@ -39,8 +39,8 @@ body {
  * It's technically possible in some browsers to offset it, but not all.
  */
 .ui-tabs .ui-tabs-nav {
-	padding-top: 110px !important;
-	background-image: url(blackhighlighter-header-logo.png);
+	padding-top: 142px !important;
+	background-image: url(blackhighlighter-header-nodejitsu.png);
 	background-position: top right;
 	background-repeat: no-repeat;
 }
@@ -69,12 +69,13 @@ div.textarea-wrapper {
     -webkit-box-sizing: border-box;
        -moz-box-sizing: border-box;
             box-sizing: border-box;
+
+    min-height: 200px;
 }
 .blackhighlighter {
 	position: relative;
 	color: black;
     background-color: white;
-    min-height: 200px;
 
 	/* I don't know if I like this or not, but it's something to think on */
 	/* border: double 6px #333; */
@@ -91,4 +92,29 @@ div.textarea-wrapper .certificate-display {
 	height: auto;
 	font-family: "Andale Mono", AndaleMono, monospace;
 	font-size: 14px;
+}
+
+div.tabs-content div.action-buttons {
+	text-align: center;
+}
+
+div.copyright, div.credits {
+	text-align: center;
+}
+
+.indeterminate-progress {
+	margin: 10px;
+}
+
+div.leading-section ul {
+	margin-left: 20px;
+	margin-right: 100px;
+}
+
+div.leading-section li {
+	margin-bottom: 8px;
+}
+
+div.action-buttons {
+	margin: 8px;
 }

public/js/blackhighlighter/blackhighlighter.css
@@ -96,3 +96,17 @@ div.blackhighlighter-show span.verified {
 div.blackhighlighter-show span.revealed {
 	background-color: #90EE90; /* Lightgreen */
 }
+
+
+/* REVEAL */
+
+div.blackhighlighter-reveal span.protected {
+	background-color: black;
+	color: black;
+}
+div.blackhighlighter-reveal span.verified {
+	background-color: red;
+}
+div.blackhighlighter-reveal span.revealed {
+	/* Don't confuse by showing already revealed info in different color */
+}

public/js/blackhighlighter/blackhighlighter.js
@@ -453,7 +453,19 @@
 					}
 
 					case 'show': {
-						throw "Cannot shift out of show mode; it's a finalization.";
+						this.$div
+							.removeClass("blackhighlighter-show");
+						break;
+					}
+
+					case 'reveal': {
+						this.$div
+							.removeClass("blackhighlighter-reveal");
+
+						clearInterval(this.blinkTimer);
+
+						this.$div.find('.placeholder.verified')
+							.css('background-color', '');
 						break;
 					}
 
@@ -503,14 +515,31 @@
 				case 'show':
 					// only good for initializing...
 					if (initializing) {
-						this.$div.addClass("blackhighlighter-show");
 						this.initialLetterText = this.$div.contents().clone();
 					} else {
-						this.$div.html(
-							"<h3>Comitting Blackhighlighter Message to Server</h3>"
-							+ "<p><i>Please wait...</i></p>"
-						);
+						// we transition to show in the editor in a broken
+						// way, but eventually we will transition in a smooth
+						// way...
 					}
+
+					this.$div.addClass("blackhighlighter-show");
+					break;
+
+				case 'reveal':
+					if (oldMode !== 'show') {
+						throw "Can only go from show to reveal.";
+					}
+					this.$div.addClass("blackhighlighter-reveal");
+
+					function blinker() {
+						this.$div.find('.placeholder.verified')
+							.css('background-color',
+								blinker.isRed ? '' : 'transparent');
+						blinker.isRed = !blinker.isRed;
+					}
+					blinker.isRed = false;
+
+					this.blinkTimer = setInterval($.proxy(blinker, this), 800);
 					break;
 
 				default:
@@ -1220,6 +1249,13 @@
 						instance.commit = temp.commit;
 						instance.protections = temp.protections;
 
+						// Technically we shouldn't have to do this, but if
+						// we don't then since we have an artificial delay
+						// the setMode call will show junk otherwise.
+						instance.$div.html(
+							"<h3>Comitting Blackhighlighter Message to Server</h3>"
+							+ "<p><i>Please wait...</i></p>"
+						);
 						instance.setMode('show');
 
 						callback(null);
@@ -1482,7 +1518,7 @@
 
 				var instance = Blackhighlighter.getInstance(this.get(0));
 
-				if (instance.mode === 'show') {
+				if ((instance.mode === 'show') || (instance.mode === 'reveal')) {
 					// Don't return the actual commit object!
 					return _.clone(instance.commit);
 				} else {
@@ -1496,7 +1532,7 @@
 			if (arg1 === "protections") {
 				if (!instance) return undefined;
 
-				if (instance.mode === 'show') {
+				if ((instance.mode === 'show') || (instance.mode === 'reveal')) {
 					// Don't return the actual protection objects!
 					return _.clone(instance.protections);
 				} else {
@@ -1510,7 +1546,7 @@
 			if (arg1 === "reveals") {
 				if (!instance) return undefined;
 
-				if (instance.mode === 'show') {
+				if ((instance.mode === 'show') || (instance.mode === 'reveal')) {
 					// Don't return the actual reveal objects!
 					return _.clone(instance.reveals);
 				} else {
@@ -1529,7 +1565,7 @@
 
 			var instance = Blackhighlighter.getInstance(this.get(0));
 
-			if (instance.mode === 'show') {
+			if ((instance.mode === 'show') || (instance.mode === 'reveal')) {
 				// Once in the show state, it's too late to make changes
 				// But does adding reveals count as a modification?
 				return false;

public/js/jquery-actual/jquery.actual.js
@@ -1,0 +1,106 @@
+/*! Copyright 2012, Ben Lin (http://dreamerslab.com/)
+ * Licensed under the MIT License (LICENSE.txt).
+ *
+ * Version: 1.0.16
+ *
+ * Requires: jQuery >= 1.2.3
+ */
+ (function(factory) { 
+  // Add jQuery via AMD registration or browser globals
+  if (typeof define === 'function' && define.amd) {
+    define([ 'jquery' ], factory);
+  }
+  else {
+    factory(jQuery);
+  }
+}( function ( $ ){
+  $.fn.addBack = $.fn.addBack || $.fn.andSelf;
+
+  $.fn.extend({
+
+    actual : function ( method, options ){
+      // check if the jQuery method exist
+      if( !this[ method ]){
+        throw '$.actual => The jQuery method "' + method + '" you called does not exist';
+      }
+
+      var defaults = {
+        absolute      : false,
+        clone         : false,
+        includeMargin : false
+      };
+
+      var configs = $.extend( defaults, options );
+
+      var $target = this.eq( 0 );
+      var fix, restore;
+
+      if( configs.clone === true ){
+        fix = function (){
+          var style = 'position: absolute !important; top: -1000 !important; ';
+
+          // this is useful with css3pie
+          $target = $target.
+            clone().
+            attr( 'style', style ).
+            appendTo( 'body' );
+        };
+
+        restore = function (){
+          // remove DOM element after getting the width
+          $target.remove();
+        };
+      }else{
+        var tmp   = [];
+        var style = '';
+        var $hidden;
+
+        fix = function (){
+          // get all hidden parents
+          $hidden = $target.parents().addBack().filter( ':hidden' );
+          style   += 'visibility: hidden !important; display: block !important; ';
+
+          if( configs.absolute === true ) style += 'position: absolute !important; ';
+
+          // save the origin style props
+          // set the hidden el css to be got the actual value later
+          $hidden.each( function (){
+            // Save original style. If no style was set, attr() returns undefined
+            var $this     = $( this );
+            var thisStyle = $this.attr( 'style' );
+
+            tmp.push( thisStyle );
+            // Retain as much of the original style as possible, if there is one
+            $this.attr( 'style', thisStyle ? thisStyle + ';' + style : style );
+          });
+        };
+
+        restore = function (){
+          // restore origin style values
+          $hidden.each( function ( i ){
+            var $this = $( this );
+            var _tmp  = tmp[ i ];
+
+            if( _tmp === undefined ){
+              $this.removeAttr( 'style' );
+            }else{
+              $this.attr( 'style', _tmp );
+            }
+          });
+        };
+      }
+
+      fix();
+      // get the actual value with user specific methed
+      // it can be 'width', 'height', 'outerWidth', 'innerWidth'... etc
+      // configs.includeMargin only works for 'outerWidth' and 'outerHeight'
+      var actual = /(outer)/.test( method ) ?
+        $target[ method ]( configs.includeMargin ) :
+        $target[ method ]();
+
+      restore();
+      // IMPORTANT, this plugin only return the value of the first element
+      return actual;
+    }
+  });
+}));

public/read.js
@@ -27,7 +27,8 @@ define([
 	// these libs have no results, purely additive...
 	'jqueryui',
 	'expanding',
-	'blackhighlighter'
+	'blackhighlighter',
+	'actual'
 ], function($, _, common, clientCommon) {
 	
 	var Globals = {
@@ -69,7 +70,12 @@ define([
 	// Bring tabs to life.
 	$('#tabs').tabs();
 	
-	function notifyErrorOnTab(tab, err) {
+	$(window).resize(clientCommon.resizeListener);
+
+	clientCommon.resizeListener(null);
+
+	function notifyErrorOnTab(tabname, err) {
+		var $tab = $("#tabs-" + tabname);
 		var message = "<span><b>" + err.toString() + "</b></span>";
 
 		if (err instanceof Error) {
@@ -81,12 +87,13 @@ define([
 				+ 'Please save a copy of this error and report it to <a href="https://github.com/hostilefork/blackhighlighter/issues/new">the Blackhighlighter Issue Tracker</a> on GitHub!';
 		} 
 
-		$('#error-' + tab + '-msg').empty().html(message);
-		$('#error-' + tab).show();
+		$tab.find(".error-display-msg").empty().html(message);
+		$tab.find('.error-display').show();
 	}
 	
-	function clearErrorOnTab(tab) {
-		$('#error-' + tab).hide();
+	function clearErrorOnTab(tabname) {
+		var $tab = $("#tabs-" + tabname);
+		$tab.find('.error-display').hide();
 	}
 
 /*
@@ -385,6 +392,8 @@ define([
 	// Bind function for what happens on tab select
 	$('#tabs').on('tabsbeforeactivate', function(event, ui) {
 
+		var $editor = $("#editor");
+
 		switch(ui.newPanel.attr('id')) {
 			case 'tabs-verify':
 				$('#progress-verify').hide();
@@ -392,10 +401,19 @@ define([
 				break;
 			
 			case 'tabs-show':		
-				// Shouldn't have to do anything?		
+				// Shouldn't have to do anything?
+				$("#tabs-show .textarea-wrapper").append(
+					$editor.remove()
+				);
+				$editor.blackhighlighter('option', 'mode', 'show');
 				break;
 		
 			case 'tabs-reveal':
+				$("#tabs-reveal .textarea-wrapper").append(
+					$editor.remove()
+				);
+				$editor.blackhighlighter('option', 'mode', 'reveal');
+
 				clearErrorOnTab('reveal');
 				$('#progress-reveal').hide();
 

commit e1d3443afa1b9bf97049fbbd77a28f462aaacd32

Fix regression caused by 36ff693 (title was being used for more than display!)

app.js
@@ -418,7 +418,8 @@ function generateHtmlFromCommitAndReveals(commit, reveals) {
 				// more clever?  e.g. we could add a method onto the element
 				// or keep a sidestructure
 				var placeholder = 
-					'<span class="placeholder protected">'
+					'<span class="placeholder protected" title="'
+					+ commitSpan.sha256 + '">'
 					+ placeholderString + '</span>';
 
 				result += placeholder;